b36f5861fcab0f48a40face4f8b851ef160dcc1b
mercy131 <darkacorn949@gmail.com>
2023-07-06 16:24:40 +0300

    Revert "init"
    This reverts commit f42a9a77f35e5c75602c23c3408ceccc8ecf9ef1.

--- src/Decorator.cpp
+++
@@ -1,9 +0,0 @@
-#include "Decorator.h"
-/*Some game upds that needs to up its price*/
-void DecoratorUpdOne::AddNewUpdate() {
-	gm->UpPrice(600);
-}
-
-void DecoratorUpdTwo::AddNewUpdate() {
-	gm->UpPrice(800);
-}
